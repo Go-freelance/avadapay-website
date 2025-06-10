e1f5b97f94a6c30f6151a5972eec82b16ee7ab0f
Daniel Mwamba <danmuamba81@gmail.com>
2025-06-10 09:57:50 +0100

    feat: add script google analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Mulish } from "next/font/google";
 import { getI18n } from "@/locales/server";
 import { I18nProviderClient } from "@/locales/client";
+import Script from "next/script";
 import "../globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -30,6 +31,20 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} className={`${mulish.variable} scroll-smooth`}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-E7ZJCLFBL4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-E7ZJCLFBL4');
+          `}
+        </Script>
+      </head>
       <body className="font-mulish antialiased overflow-x-hidden">
         <I18nProviderClient locale={locale}>
           {children}
